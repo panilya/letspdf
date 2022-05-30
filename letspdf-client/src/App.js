d5c68be5cfb3165e81f00c4f4e0779fb452e4370
panilya <panilyau@gmail.com>
2022-05-30 16:01:23 +0300

    Frontend implementation of sending images to server

--- letspdf-client/src/App.js
+++ letspdf-client/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    files: null,
+  };
+
+  handleFile(e) {
+    let files = e.target.files;
+    this.setState({ files });
+  }
+
+  handleUpload(e) {
+      let files = this.state.files;
+
+      let formData = new FormData();
+
+      for (let i = 0; i < files.length; i++) {
+          formData.append("files", files[i])
+      }
+
+      axios.post("http://localhost:8082/api/v1/upload", formData,
+          {
+              headers: {
+                  "Content-Type": "multipart/form-data"
+              }
+          }).then(() => {
+          console.log("success")
+      }).catch(err => {
+          console.log(err.status)
+      })
+  }
+  render() {
+    return (
+        <div>
+          <h1>Select your files</h1>
+          <input
+              type="file"
+              multiple="multiple"
+              onChange={(e) => this.handleFile(e)}
+          />
+          <button onClick={(e) => this.handleUpload(e)}
+          >Send Files</button>
+        </div>
+    );
+  }
 }
 
 export default App;
