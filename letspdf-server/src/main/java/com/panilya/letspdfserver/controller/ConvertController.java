fb07cdc09b148c78d3192beb428f6534af0f8736
panilya <panilyau@gmail.com>
2022-05-30 16:03:11 +0300

    Add Conversion controller

--- letspdf-server/src/main/java/com/panilya/letspdfserver/controller/ConvertController.java
+++ letspdf-server/src/main/java/com/panilya/letspdfserver/controller/ConvertController.java
@@ -1,19 +1,29 @@
 package com.panilya.letspdfserver.controller;
 
+import com.panilya.letspdfserver.service.ImageToPdfConversionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin("*")
 public class ConvertController {
 
-    @PostMapping("/upload")
-    public ResponseEntity<?> uploadFiles(@RequestParam("file") MultipartFile[] files) {
+    private final ImageToPdfConversionService imageToPdfConversionService;
 
+    @Autowired
+    public ConvertController(ImageToPdfConversionService imageToPdfConversionService) {
+        this.imageToPdfConversionService = imageToPdfConversionService;
     }
 
+    @PostMapping(path = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void uploadFiles(@RequestParam("files") MultipartFile[] files) throws IOException {
+        imageToPdfConversionService.convert(files);
+    }
 }
