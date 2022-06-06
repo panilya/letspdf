0b3b0a837baee1ed547570954e1cacd3f36a68ba
panilya <panilyau@gmail.com>
2022-06-06 19:10:56 +0300

    Open up Save As dialog on response in order to save PDF file

--- letspdf-client/src/App.js
+++ letspdf-client/src/App.js
@@ -26,10 +26,15 @@ class App extends React.Component {
           {
               headers: {
                   "Content-Type": "multipart/form-data"
-              }
-          }).then(() => {
-          console.log("success")
-      }).catch(err => {
+              }, responseType: 'arraybuffer'
+          }).then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'file.pdf'); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+          }).catch(err => {
           console.log(err.status)
       })
   }
