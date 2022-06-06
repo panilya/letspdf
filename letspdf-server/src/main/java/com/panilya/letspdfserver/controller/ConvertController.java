e973c111cd4310533be4ecbab72f4372dd85e662
panilya <panilyau@gmail.com>
2022-06-06 19:17:46 +0300

    Refactor Convert controller, added Content-Disposition header

--- letspdf-server/src/main/java/com/panilya/letspdfserver/controller/ConvertController.java
+++ letspdf-server/src/main/java/com/panilya/letspdfserver/controller/ConvertController.java
@@ -2,6 +2,7 @@ package com.panilya.letspdfserver.controller;
 
 import com.panilya.letspdfserver.service.ImageToPdfConversionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,15 @@ public class ConvertController {
 
     @PostMapping(path = "/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void uploadFiles(@RequestParam("files") MultipartFile[] files) throws IOException {
-        imageToPdfConversionService.convert(files);
+    public ResponseEntity<byte[]> uploadFiles(@RequestParam("files") MultipartFile[] files) throws IOException {
+        byte[] pdfFile = imageToPdfConversionService.convert(files);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Content-Disposition");
+        httpHeaders.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "file.pdf" + "\"");
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(MediaType.APPLICATION_PDF_VALUE))
+                .headers(httpHeaders)
+                .body(pdfFile);
     }
 }
