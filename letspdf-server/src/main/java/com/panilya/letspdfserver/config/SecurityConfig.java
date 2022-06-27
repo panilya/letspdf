1c4de5d04dbba99c2e7ba4ba13025c9523efd095
Ilya Pantsyr <panilyau@gmail.com>
2022-06-27 20:43:00 +0300

    Clean imports in SecurityConfig

--- letspdf-server/src/main/java/com/panilya/letspdfserver/config/SecurityConfig.java
+++ letspdf-server/src/main/java/com/panilya/letspdfserver/config/SecurityConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.MultipartFilter;
 
